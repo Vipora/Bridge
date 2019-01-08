608c2534fcdb5139053deaaa31f41970345aeb84
Anton Pohli <anton.pohli@gmail.com>
2019-01-08 11:42:34 +0100

    Added GUI for configuration

---
+++ Bridge/GUI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bridge
+{
+  public partial class GUI : Form
+  {
+    public static readonly Dictionary<LeagueClientState, Icon> icons = new Dictionary<LeagueClientState, Icon>()
+    {
+      { LeagueClientState.NotRunning, Bridge.Properties.Resources.NotRunningStateIcon },
+      { LeagueClientState.ProcessFound, Bridge.Properties.Resources.ReadyToConnectStateIcon },
+      { LeagueClientState.ReadyToConnect, Bridge.Properties.Resources.ReadyToConnectStateIcon },
+      { LeagueClientState.Connected, Bridge.Properties.Resources.RunningStateIcon }
+    };
+
+    public static Icon GetIcon(LeagueClientState state = LeagueClientState.Unkown)
+    {
+      return icons.ContainsKey(state) ? icons[state] : Bridge.Properties.Resources.ErrorStateIcon;
+    }
+
+    private NotifyIcon trayIcon;
+    private ContextMenu contextMenu;
+    public GUI()
+    {
+      InitializeComponent();
+      this.trayIcon = new NotifyIcon();
+      this.trayIcon.Icon = GetIcon(LeagueClientState.NotRunning);
+      this.trayIcon.Text = "Vipora";
+      this.trayIcon.DoubleClick += TrayIcon_DoubleClick;
+      this.contextMenu = new System.Windows.Forms.ContextMenu();
+      var exitMenuItem = new System.Windows.Forms.MenuItem();
+
+      // Initialize contextMenu
+      this.contextMenu.MenuItems.AddRange(
+                  new System.Windows.Forms.MenuItem[] { exitMenuItem });
+
+      // Initialize Exit menu item
+      exitMenuItem.Index = 0;
+      exitMenuItem.Text = "E&xit";
+      exitMenuItem.Click += ExitButton_Click;
+
+      this.trayIcon.ContextMenu = this.contextMenu;
+
+      this.ShowInTaskbar = false;
+      this.WindowState = FormWindowState.Minimized;
+
+      this.FormClosing += GUI_FormClosing;
+    }
+
+    public void UpdateLeagueClientState(LeagueClientState state)
+    {
+      this.trayIcon.Icon = GetIcon(state);
+    }
+
+    public void SetTrayIconVisibility(bool visible)
+    {
+      this.trayIcon.Visible = visible;
+    }
+
+    private void ExitButton_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void TrayIcon_DoubleClick(object sender, EventArgs e)
+    {
+      // Set the WindowState to normal if the form is minimized.
+      if (this.WindowState == FormWindowState.Minimized)
+        this.WindowState = FormWindowState.Normal;
+
+      // Activate the form.
+      this.Activate();
+    }
+
+    private void GUI_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      if (e.CloseReason == CloseReason.UserClosing)
+      {
+        e.Cancel = true;
+        Hide();
+      }
+    }
+  }
+}
--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Bridge
@@ -9,20 +6,6 @@ namespace Bridge
 
   class Program
   {
-
-    public static readonly Dictionary<LeagueClientState, Icon> icons = new Dictionary<LeagueClientState, Icon>()
-    {
-      { LeagueClientState.NotRunning, Bridge.Properties.Resources.NotRunningStateIcon },
-      { LeagueClientState.ProcessFound, Bridge.Properties.Resources.ReadyToConnectStateIcon },
-      { LeagueClientState.ReadyToConnect, Bridge.Properties.Resources.ReadyToConnectStateIcon },
-      { LeagueClientState.Connected, Bridge.Properties.Resources.RunningStateIcon }
-    };
-
-    public static Icon GetIcon(LeagueClientState state = LeagueClientState.Unkown)
-    {
-      return icons.ContainsKey(state) ? icons[state] : Bridge.Properties.Resources.ErrorStateIcon;
-    }
-
     static void Main(string[] args)
     {
       var program = new Program();
@@ -30,40 +13,19 @@ namespace Bridge
     }
 
     private NotifyIcon trayIcon;
-    private LeagueClient client;
     private ContextMenu contextMenu;
+    private LeagueClient client;
+    private GUI gui;
 
     public Program()
     {
-      this.trayIcon = new NotifyIcon();
-      this.trayIcon.Icon = GetIcon(LeagueClientState.NotRunning);
-      this.trayIcon.Text = "Vipora";
-      this.contextMenu = new System.Windows.Forms.ContextMenu();
-      var exitMenuItem = new System.Windows.Forms.MenuItem();
-
-      // Initialize contextMenu1
-      this.contextMenu.MenuItems.AddRange(
-                  new System.Windows.Forms.MenuItem[] { exitMenuItem });
-
-      // Initialize menuItem1
-      exitMenuItem.Index = 0;
-      exitMenuItem.Text = "E&xit";
-      exitMenuItem.Click += ExitButton_Click;
-
-      this.trayIcon.ContextMenu = this.contextMenu;
-
       this.client = new LeagueClient();
       this.client.LeagueClientStateChanged += Client_LeagueClientStateChanged;
     }
 
-    private void ExitButton_Click(object sender, EventArgs e)
-    {
-      Application.Exit();
-    }
-
     private void Client_LeagueClientStateChanged(object sender, ClientStateChangedEventArgs e)
     {
-      this.trayIcon.Icon = GetIcon(e.State);
+      this.gui.UpdateLeagueClientState(e.State);
     }
 
     public async Task Run()
@@ -71,9 +33,12 @@ namespace Bridge
       client.Connect();
       await Task.Run(() =>
       {
-        this.trayIcon.Visible = true;
-        Application.Run();
-        this.trayIcon.Visible = false;
+        // Create the Gui here to cricumvent some threading issues
+        // TODO: Find a better solution
+        this.gui = new GUI();
+        this.gui.SetTrayIconVisibility(true);
+        Application.Run(this.gui);
+        this.gui.SetTrayIconVisibility(false);
       });
     }
 
