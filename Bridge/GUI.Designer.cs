5d0811610f9420d5bcb2d4c76a254df389e55a13
Anton Pohli <anton.pohli@gmail.com>
2019-01-08 14:19:00 +0100

    Added Gui content

--- Bridge/GUI.Designer.cs
+++ Bridge/GUI.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "GUI";
+      this.exitBtn = new System.Windows.Forms.Button();
+      this.saveBtn = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.errorLabel = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // exitBtn
+      // 
+      this.exitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(192)))), ((int)(((byte)(181)))));
+      this.exitBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.exitBtn.Location = new System.Drawing.Point(23, 398);
+      this.exitBtn.Name = "exitBtn";
+      this.exitBtn.Size = new System.Drawing.Size(96, 29);
+      this.exitBtn.TabIndex = 1;
+      this.exitBtn.Text = "Exit Vipora";
+      this.exitBtn.UseVisualStyleBackColor = false;
+      this.exitBtn.Click += new System.EventHandler(this.ExitButton_Click);
+      // 
+      // saveBtn
+      // 
+      this.saveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(192)))), ((int)(((byte)(181)))));
+      this.saveBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.saveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.saveBtn.Location = new System.Drawing.Point(288, 398);
+      this.saveBtn.Name = "saveBtn";
+      this.saveBtn.Size = new System.Drawing.Size(96, 29);
+      this.saveBtn.TabIndex = 2;
+      this.saveBtn.Text = "Save";
+      this.saveBtn.UseVisualStyleBackColor = false;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(20, 83);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(101, 17);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Access Token:";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(168, 83);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(216, 22);
+      this.textBox1.TabIndex = 4;
+      // 
+      // errorLabel
+      // 
+      this.errorLabel.Location = new System.Drawing.Point(23, 288);
+      this.errorLabel.Name = "errorLabel";
+      this.errorLabel.Size = new System.Drawing.Size(361, 78);
+      this.errorLabel.TabIndex = 5;
+      this.errorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // GUI
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(240)))), ((int)(((byte)(237)))));
+      this.ClientSize = new System.Drawing.Size(430, 450);
+      this.Controls.Add(this.errorLabel);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.saveBtn);
+      this.Controls.Add(this.exitBtn);
+      this.ForeColor = System.Drawing.SystemColors.Window;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Name = "GUI";
+      this.ShowInTaskbar = false;
+      this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+      this.Text = "Vipora";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
         }
 
-        #endregion
-    }
+    #endregion
+    private System.Windows.Forms.Button exitBtn;
+    private System.Windows.Forms.Button saveBtn;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label errorLabel;
+  }
 }
--- Bridge/GUI.cs
+++ Bridge/GUI.cs
@@ -7,6 +7,14 @@ namespace Bridge
 {
   public partial class GUI : Form
   {
+    private static readonly Color c_lightest = Color.FromArgb(0xE6, 0xE8, 0xFF);
+    private static readonly Color c_lighter = Color.FromArgb(0xB2, 0xB7, 0xFF);
+    private static readonly Color c_light = Color.FromArgb(0x78, 0x86, 0xD7);
+    private static readonly Color c_base = Color.FromArgb(0x65, 0x74, 0xCD);
+    private static readonly Color c_dark = Color.FromArgb(0x56, 0x61, 0xB3);
+    private static readonly Color c_darker = Color.FromArgb(0x2F, 0x36, 0x5F);
+    private static readonly Color c_darkest = Color.FromArgb(0x19, 0x1E, 0x38);
+
     public static readonly Dictionary<LeagueClientState, Icon> icons = new Dictionary<LeagueClientState, Icon>()
     {
       { LeagueClientState.NotRunning, Bridge.Properties.Resources.NotRunningStateIcon },
@@ -25,21 +33,32 @@ namespace Bridge
     public GUI()
     {
       InitializeComponent();
+      this.BackColor = c_darker;
+      this.saveBtn.BackColor = c_base;
+      this.exitBtn.BackColor = c_base;
+
+
       this.trayIcon = new NotifyIcon();
       this.trayIcon.Icon = GetIcon(LeagueClientState.NotRunning);
       this.trayIcon.Text = "Vipora";
       this.trayIcon.DoubleClick += TrayIcon_DoubleClick;
       this.contextMenu = new System.Windows.Forms.ContextMenu();
       var exitMenuItem = new System.Windows.Forms.MenuItem();
+      var optionsMenuItem = new System.Windows.Forms.MenuItem();
 
       // Initialize contextMenu
       this.contextMenu.MenuItems.AddRange(
-                  new System.Windows.Forms.MenuItem[] { exitMenuItem });
+                  new System.Windows.Forms.MenuItem[] { exitMenuItem, optionsMenuItem });
 
       // Initialize Exit menu item
-      exitMenuItem.Index = 0;
+      exitMenuItem.Index = 1;
       exitMenuItem.Text = "E&xit";
       exitMenuItem.Click += ExitButton_Click;
+
+      // Initialize Options menu item
+      optionsMenuItem.Index = 0;
+      optionsMenuItem.Text = "Options";
+      optionsMenuItem.Click += TrayIcon_DoubleClick;
 
       this.trayIcon.ContextMenu = this.contextMenu;
 
